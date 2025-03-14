717eb3a26c44307937cf1a3f405f75ca50e9f9d9
陳德生 <111033412+KoukeNeko@users.noreply.github.com>
2025-03-14 08:42:40 +0800

    Refactor command system initialization in KoukeNekoKO class

--- src/main/java/dev/doeshing/koukeNekoKO/KoukeNekoKO.java
+++ src/main/java/dev/doeshing/koukeNekoKO/KoukeNekoKO.java
@@ -5,13 +5,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class KoukeNekoKO extends JavaPlugin {
 
-    private CommandSystem commandSystem;
-
     @Override
     public void onEnable() {
 
         // 使用 CommandSystem 註冊 reload 指令
-        commandSystem = new CommandSystem(this);
+        CommandSystem commandSystem = new CommandSystem(this);
         commandSystem.registerCommand("koukenekokoreload", new dev.doeshing.koukeNekoKO.commands.ReloadCommand(this), "koukeneko.ko.admin", "重載插件設定", "/koukenekokoreload", "knkoreload");
         // 若 config.yml 尚未存在則複製預設配置
         saveDefaultConfig();
